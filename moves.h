fd1caf3df359ed20f09c89a656421f89ee72daf2
Phantm0133 <93774127+Phantm0133@users.noreply.github.com>
2024-11-25 22:06:22 +0530

    Create moves.h

---
+++ moves.h
@@ -0,0 +1,9 @@
+#ifndef MOVES_H
+#define MOVES_H
+
+void moveUp(int board[4][4], int *cscr);
+void moveDown(int board[4][4], int *cscr);
+void moveRight(int board[4][4], int *cscr);
+void moveLeft(int board[4][4], int *cscr);
+
+#endif
